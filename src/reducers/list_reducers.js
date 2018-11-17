ed2e8f59dccaf7958babb97f305639bbeb84b8f8
julian so <julianso89@gmail.com>
2018-11-16 17:28:37 -0800

    Completed getting list data from server

--- src/reducers/list_reducers.js
+++ src/reducers/list_reducers.js
@@ -1,13 +1,15 @@
-import types from '../actions/types';
+import types from "../actions/types";
 
 const DEFAULT_STATE = {
-    all: [],
-    single: {}
+  all: [],
+  single: {}
 };
 
 export default (state = DEFAULT_STATE, action) => {
-    switch(action, types){
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case types.GET_ALL_LIST_DATA:
+      return { ...state, all: action.payload.data.todos };
+    default:
+      return state;
+  }
+};
